e1da163a42cae1dc64a6bb01c4dc41c90ab12b9a
Alfred Mwangangi <alfredmwangangi1996@gmail.com>
2019-02-21 19:40:05 +0300

    First files

---
+++ app/src/main/java/ke/co/digisoftsolutions/matatusacco/MainActivity.java
@@ -0,0 +1,17 @@
+package ke.co.digisoftsolutions.matatusacco;
+//imports
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+
+    }
+    }
+
